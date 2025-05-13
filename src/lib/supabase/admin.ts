c3b672341a1793d435ab42a652850ae8b8c1d0f1
vicotrbb <victor.bona@hotmail.com>
2025-05-13 00:45:51 -0300

    chore: fixed build

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -1,7 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { Database } from '@/types/database.types';
 
 export function createAdminClient() {
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+    return null;
+  }
+
   const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
@@ -18,4 +22,4 @@ export function createAdminClient() {
 
 // Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
 // as it has admin privileges and overwrites RLS policies!
-export const supabaseAdmin = createAdminClient();
+export const supabaseAdmin: SupabaseClient<Database> = createAdminClient()!;
